e9300f99f6cd67b38586f4ae39538e9818e8067f
Ian Engelbrecht <ianicus.za@gmail.com>
2022-08-12 16:41:57 +0200

    added additonal keywords for collection information

--- ImageTagger.py
+++ ImageTagger.py
@@ -1,20 +1,30 @@
 import pandas as pd
 import os 
 
+#This uses ExifTool: https://exiftool.org
+#Make sure ExifTool is installed and available on your PATH first...
+
+#SETTINGS
+
 #fields to tag images with
-tagfields = ['CONTINENT', 'COUNTRY', 'MAJORAREA', 'FAMILY', 'GENUS', 'SPECIES', 'COLLECTOR LASTNAME',  'HOMETSTAT']
+collectionname = 'National Herbarium'
+collectioncode = 'PRE'
+collectiontype = 'vascular plants' #vertebrate fossils, reptiles, insects, etc
+
+tagfields = ['CONTINENT', 'COUNTRY', 'MAJORAREA', 'FAMILY', 'GENUS', 'SPECIES', 'COLLECTOR LASTNAME']
+typefield = 'HOMETSTAT' #using this will also add the keywork 'type' to the images if this field has a value
 titlefield = "BARCODE"
 captionfield = "HOMETYPE"
+sensitivefield = None
+
 copyright = "South African National Biodiversity Institute"
 license = 'CC BY 4.0'
 licenseurl = "https://creativecommons.org/licenses/by/4.0/"
-rights = "Free to use for any purpose, including commercial purposes, with attribution. See License and AttributionName"
+rights = "Free to use for any purpose, including commercial purposes, with attribution. See License and AttributionName" #statement or URL
 attribution = "South African National Biodiversity Institute"
 attributionURL = "https://www.sanbi.org/"
 
-#TODO add sensitive column
-
-fileext = '.tif'
+fileext = '.tif' #the file types to filter on, assumes all the same
 
 ##read csv as dataframe
 csvpath = "C:\\temp\\Herbarium mass digitization project\\ImageTaggingExperiments"
@@ -25,21 +35,20 @@ image_dir = csvpath
 
 #THE SCRIPT
 
+#read the data file
 fullpath = os.path.join(csvpath, csvfile)
 df = pd.read_csv(fullpath)
 print(df.shape[0], 'records read from', csvfile)
 
-##read in images from directory
+##loop through the images and add tags
 images = os.listdir(image_dir)
 count = 0
-#tag multiple images with unique keywords
 recordsnotfound = []
 for image in images:
     if (image.endswith)(fileext):
 
         #get the exif data
         imagepath = os.path.join(image_dir, image)
-        #exif_dict = piexif.load(imagepath)
 
         #get the keywords
         imagerecord = df.loc[df['BARCODE'] == image.replace(fileext, '')]
@@ -53,13 +62,27 @@ for image in images:
         for field in tagfields:
             keywords.append(imagerecord[field].values[0])
 
-        keywords.append(license)
- 
-        #filter out nans/empty values
-        keywords = [x for x in keywords if str(x) != 'nan' and x != None]
-        #tag the image
-        keywordsstring = ",".join(keywords)
+        #for types we want to add keyword 'type' if the type is not already 'type'
+        if typefield:
+            typeval = imagerecord[typefield].values[0]
+            if typeval and str(typeval) != 'nan' and typeval.strip() != '' and typeval.lower() != 'type':
+                keywords.append('type')
 
+        if sensitivefield:
+            keywords.append(imagerecord[sensitivefield].values[0])
+
+        keywords.append(license)
+        keywords.append(collectiontype)
+        keywords.append(collectionname)
+        keywords.append(collectiontype)
+ 
+        #filter out nans/empty values and trim values
+        keywords = [x for x in keywords if str(x) != 'nan' and x!= None and x.strip() != '']
+        keywords = map(str.strip, keywords)
+
+        keywordsstring = ",".join(keywords)
+        
+        #tag the image
         exiftoolcmd = f'exiftool -title="{title}" -xmp:description="{caption}" -copyright="{copyright}" -license="{licenseurl}" -usageterms="{rights}" -attributionname="{attribution}" -attributionurl="{attributionURL}" -xmp:subject="{keywordsstring}" -sep ","  -overwrite_original "{imagepath}"'
         os.system(exiftoolcmd)
         
@@ -68,6 +91,7 @@ for image in images:
         if(count % 10 == 0):
             print(count, 'images tagged')
 
+#print any images where records not found in the dataset
 if len(recordsnotfound) > 0:
     print()
     print('Could not find records for the following images:')
