64e3904904c22a9b7623e3de1e7d7e97ff441b7d
Ian Engelbrecht <ianicus.za@gmail.com>
2022-08-20 16:19:03 +0200

    added tif compression script

---
+++ compressTIFs.py
@@ -0,0 +1,80 @@
+## compress tiff images, and only tiff images...
+## NOTE that this uses LZW compression, see https://havecamerawilltravel.com/tiff-image-compression/
+## BUT ZIP compression in Adobe Lightroom gives much better results...
+
+#IMPORTS
+from PIL import Image, features
+import os
+from time import time, strftime, gmtime
+
+#SETTINGS
+
+#the directory with the images
+image_dir = r'C:\temp\Herbarium mass digitization project\ImageTaggingExperiments'
+overwrite = False #overwrite the existing file or write to a new location. If 'new_dir' is not specified below it will add to a subdirectory called 'compressed'
+new_dir = r''
+
+#SCRIPT
+if not features.check('libtiff'):
+  print('TIFF compression requires libtiff to be installed. Please run pip install libtiff and try again...')
+  exit()
+
+#function for creating human readable file sizes
+#from https://stackoverflow.com/a/1094933/3210158
+def sizeof_fmt(num, suffix="B"):
+  for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+    if abs(num) < 1024.0:
+      return f"{num:3.1f}{unit}{suffix}"
+    num /= 1024.0
+  return f"{num:.1f}Yi{suffix}"
+
+#create the output directory if needed
+if not overwrite:
+  if len(new_dir):
+    if not os.path.isdir(new_dir):
+      try:
+        os.mkdir(new_dir)
+      except:
+        print('Please provide a valid output directory...') #the input string must not have been valid
+        exit()
+  else:
+    new_dir = os.path.join(image_dir, 'compressed')
+    if not os.path.isdir(new_dir):
+      try:
+        os.mkdir(new_dir)
+      except:
+        print('Eish! Something went wront with creating the \'compressed\' directory...') #the input string must not have been valid
+        exit()
+
+print('Starting to compress images')
+
+start = time()
+count = 0
+originalsize = 0
+finalsize = 0
+
+with os.scandir(image_dir) as it:
+  for entry in it:
+    if entry.is_file():
+      originalsize += entry.stat().st_size
+      file = entry.name
+      if file.endswith('.tif'):
+        infile = os.path.join(image_dir, file)
+        outfile = os.path.join(new_dir, file)
+
+        try:
+          with Image.open(infile) as im:
+            im.save(outfile, compression = 'tiff_lzw')
+            finalsize += os.path.getsize(outfile)
+            count += 1
+        except Exception as e:
+          message = f'cannot convert {infile}: {e}'
+          print(message)
+
+end = time()
+print('Finished compressing files')
+totaltime = strftime("%H:%M:%S", gmtime(end - start))
+print(count, 'images compressed in', totaltime)
+print('Total original size:', sizeof_fmt(originalsize))
+print('Total compressed size:', sizeof_fmt(finalsize))
+
